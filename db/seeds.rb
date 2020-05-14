cee16ea7aa72fc12144dca0b5111993c22bd2831
Gala Anania <gsanania@gmail.com>
2020-05-14 14:08:09 +0200

    gemfiles, routes, CocktailsController

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'gin')
+Ingredient.create(name: 'whisky')
+Ingredient.create(name: 'coke')
+Ingredient.create(name: 'orange juice')
+Ingredient.create(name: 'cranberry juice')
+Ingredient.create(name: 'champagne')
+Ingredient.create(name: 'bourbon')
+Ingredient.create(name: 'tequila')
+Ingredient.create(name: 'lemon juice')
+Ingredient.create(name: 'vodka')
