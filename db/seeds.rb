d5a3b178a7f2f7b7750668f7f40cbf368a9119e8
Gala Anania <gsanania@gmail.com>
2020-05-15 12:51:49 +0200

    new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
+require 'json'
+require 'open-uri'
+
 Ingredient.destroy_all
 
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'gin')
-Ingredient.create(name: 'whisky')
-Ingredient.create(name: 'coke')
-Ingredient.create(name: 'orange juice')
-Ingredient.create(name: 'cranberry juice')
-Ingredient.create(name: 'champagne')
-Ingredient.create(name: 'bourbon')
-Ingredient.create(name: 'tequila')
-Ingredient.create(name: 'lemon juice')
-Ingredient.create(name: 'vodka')
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url_serialized = open(url).read
+ingredients = JSON.parse(url_serialized)
+ingredients_array = ingredients['drinks']
+ingredients_array.each do |ingredient|
+  ingredient_name = ingredient['strIngredient1']
+  Ingredient.create(name: ingredient_name)
+end
